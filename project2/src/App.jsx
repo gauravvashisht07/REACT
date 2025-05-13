3db0920a1fe605fa5e32506e19754f12fb03500b
Gaurav <gs177137@gmail.com>
2025-05-14 00:34:35 +0530

    tailwind and props passing

---
+++ project2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  
+
+  return (
+    <>
+     <Card username="chai"  price="1"/>
+    </>
+  )
+}
+
+export default App
